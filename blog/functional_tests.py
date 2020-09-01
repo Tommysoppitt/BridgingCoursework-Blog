f12942d4a7aa1b3170cbf9aa3bb5c024bdab8550
Tom Soppitt <soppitttom@gmail.com>
2020-09-01 01:49:01 +0100

    Added tests for TDD

--- blog/functional_tests.py
+++ blog/functional_tests.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from django import forms
 import unittest
+
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 
 class NewVisitorTest(unittest.TestCase):  
 
@@ -9,17 +13,34 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://tommysoppitt.pythonanywhere.com')
+    def test_home_page(self):  
+        # Tom visits his new online CV
+        self.browser.get('http://tommysoppitt.pythonanywhere.com/cv')
 
-        # She notices the page title and header mention to-do lists
-        self.assertIn('Tom', self.browser.title)  
-        self.fail('Finish the test!')  
+        # He notices the title and header contains the words "Tom Soppitt's CV"
+        self.assertIn("Tom Soppitt's CV", self.browser.title)  
+        header_text = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn("Tom Soppitt's CV", header_text)
 
-        # She is invited to enter a to-do item straight away
-      
+        # He wants to add a new skill to the CV
+    def test_new_skill_page(self):  
+       
+        self.browser.get('http://tommysoppitt.pythonanywhere.com/cv/skill_new')
+     # He wants to add a new experience to the CV
+    def test_new_experience_page(self):  
+       
+        self.browser.get('http://tommysoppitt.pythonanywhere.com/cv/experience_new')
+# He wants to add a new position of responsibility to the CV
+    def test_new_responsibility_page(self):  
+       
+        self.browser.get('http://tommysoppitt.pythonanywhere.com/cv/responsibility_new')
+# He wants to add a new hobby to the CV
+    def test_new_hobby_page(self):  
+       
+        self.browser.get('http://tommysoppitt.pythonanywhere.com/cv/hobby_new')
+
+
+        
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.urls import resolve
 from blog.views import cv
-
+from blog.views import skill_new
+from blog.views import hobby_new
+from blog.views import responsibility_new
+from blog.views import experience_new
+from django.test import TestCase
+from .forms import SkillForm
+from django.http import HttpResponse
+from django.http import HttpRequest
 
 class cvPageTest(TestCase):
 
@@ -9,10 +16,54 @@ class cvPageTest(TestCase):
         found = resolve('/cv')  
         self.assertEqual(found.func,cv)  
 
-def test_cv_page_returns_correct_html(self):
+    def test_cv_page_returns_correct_html(self):
         request = HttpRequest()  
         response = cv(request)  
         html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))  
-        self.assertIn('<title>cv</title>', html)  
+     
+        self.assertIn("Tom Soppitt's CV", html)  
         self.assertTrue(html.endswith('</html>'))  
+
+       
+    def test_new_skill_url_resolves_to_new_skill_view(self):
+        found = resolve('/cv/skill_new')  
+        self.assertEqual(found.func,skill_new)  
+
+    def test_new_skill_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = skill_new(request)  
+        html = response.content.decode('utf8')  
+       
+        self.assertIn('<h2>New post</h2>', html)  
+        
+    def test_new_responsibility_url_resolves_to_new_responsibility_view(self):
+        found = resolve('/cv/responsibility_new')  
+        self.assertEqual(found.func,responsibility_new)  
+
+    def test_new_responsibility_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = responsibility_new(request)  
+        html = response.content.decode('utf8')  
+     
+        self.assertIn('<h2>New post</h2>', html)  
+      
+    
+    def test_new_experience_url_resolves_to_new_experience_view(self):
+        found = resolve('/cv/experience_new')  
+        self.assertEqual(found.func,experience_new)  
+
+    def test_new_experience_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = experience_new(request)  
+        html = response.content.decode('utf8')  
+        self.assertIn('<h2>New post</h2>', html)  
+
+    def test_new_hobby_url_resolves_to_new_hobby_view(self):
+        found = resolve('/cv/hobby_new')  
+        self.assertEqual(found.func,hobby_new)  
+
+    def test_new_hobby_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = hobby_new(request)  
+        html = response.content.decode('utf8')  
+        self.assertIn('<h2>New post</h2>', html)  
